4d150ed8cb2f1884f30929a69e567709fe43e66f
Vicente Escarate <vicente.escarate.l@mail.pucv.cl>
2024-10-08 22:16:45 -0300

    Co-authored-by: MatiasCeledon420 <MatiasCeledon420@users.noreply.github.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,43 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonItem, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import '../theme/Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+        <IonToolbar className="custom">
+          <IonTitle>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Login</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+        <IonItem>
+          <IonToolbar className='Titulo-Correo'>
+            <IonTitle className='.CCentrado'>Iniciar sesion</IonTitle>
+          </IonToolbar>
+        </IonItem>
+        <IonItem>
+          <IonTextarea  placeholder="Ingresa tu correo electrónico"
+                        className="semi-transparent-text"></IonTextarea>
+        </IonItem>
+        <IonItem>
+          <IonToolbar>
+            <IonTextarea  placeholder="Ingresa tu Contraseña"
+                        className="semi-transparent-text"></IonTextarea>
+          </IonToolbar>
+        </IonItem>
+        <IonItem>
+          <IonToolbar className="botones-container">
+            <IonButton className="Button-Iniciar">Iniciar</IonButton>
+            <IonButton className="Button-CrearCuenta">Crear cuenta</IonButton>
+          </IonToolbar>
+        </IonItem>
       </IonContent>
     </IonPage>
   );
